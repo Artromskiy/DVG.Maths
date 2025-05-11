de529e285d20fea1f622ed053128ed8a1c04c4c2
Artromskiy <47901401+Artromskiy@users.noreply.github.com>
2025-05-11 20:04:26 +0300

    Aggressive inlining and Angle extended functions

--- Maths.Extended.cs
+++ Maths.Extended.cs
@@ -1,18 +1,13 @@
-﻿namespace DVG
+﻿using System;
+
+namespace DVG
 {
     public partial class Maths
     {
-        public static float SmoothDampAngle(float source, float target, ref float velocity, float smoothTime, float deltaTime)
-        {
-            var delta = 180 - Maths.Abs(Maths.Abs(source - target) - 180);
-            target = source + delta;
-            return SmoothDamp(source, target, ref velocity, smoothTime, deltaTime);
-        }
-
-        public static float SmoothDamp(float source, float target, ref float velocity, float smoothTime, float deltaTime)
+        public static float SmoothDamp(float current, float target, ref float velocity, float smoothTime, float deltaTime)
         {
             float omega = 2f / smoothTime;
-            float delta = source - target;
+            float delta = current - target;
             float x = deltaTime * omega;
             float exp = 1f / (1f + x + (x * x * ((x * 0.235f) + 0.48f)));
             float temp = (velocity * deltaTime) + (x * delta);
@@ -24,10 +19,10 @@
             return final;
         }
 
-        public static double SmoothDamp(double source, double target, ref double velocity, double smoothTime, double deltaTime)
+        public static double SmoothDamp(double current, double target, ref double velocity, double smoothTime, double deltaTime)
         {
             double omega = 2.0 / smoothTime;
-            double delta = source - target;
+            double delta = current - target;
             double x = deltaTime * omega;
             double exp = 1.0 / (1.0 + x + (x * x * ((x * 0.235) + 0.48)));
             double temp = (velocity * deltaTime) + (x * delta);
@@ -37,13 +32,6 @@
             double final = finalized ? target : target + move;
             velocity = finalized ? 0 : velocity;
             return final;
-        }
-
-        public static double SmoothDampAngle(double source, double target, ref double velocity, double smoothTime, double deltaTime)
-        {
-            var delta = 180 - Maths.Abs(Maths.Abs(source - target) - 180);
-            target = source + delta;
-            return SmoothDamp(source, target, ref velocity, smoothTime, deltaTime);
         }
 
         public static float InvLerp(float edge0, float edge1, float value) => (value - edge0) / (edge1 - edge0);
@@ -66,5 +54,48 @@
             double min = Min(maxDelta, Abs(delta));
             return delta > 0 ? current + min : current - min;
         }
+
+        public static float RotateTowards(float current, float target, float maxStep)
+        {
+            current = WrapAngle(current);
+            target = WrapAngle(target);
+            float delta = target - current;
+            delta -= (360 * (int)((delta + 180) / 360));
+            float step = Clamp(delta, -maxStep, maxStep);
+            float result = current + step;
+            return ((result % 360) + 360) % 360;
+        }
+
+        public static double RotateTowards(double current, double target, double maxStep)
+        {
+            current = WrapAngle(current);
+            target = WrapAngle(target);
+            double delta = target - current;
+            delta -= (360 * (int)((delta + 180) / 360));
+            double step = Clamp(delta, -maxStep, maxStep);
+            double result = current + step;
+            return ((result % 360) + 360) % 360;
+        }
+
+        public static float SmoothDampAngle(float current, float target, ref float velocity, float smoothTime, float deltaTime)
+        {
+            current = WrapAngle(current);
+            target = WrapAngle(target);
+            var delta = 180 - Abs(Abs(current - target) - 180);
+            target = current + delta;
+            return SmoothDamp(current, target, ref velocity, smoothTime, deltaTime);
+        }
+
+        public static double SmoothDampAngle(double current, double target, ref double velocity, double smoothTime, double deltaTime)
+        {
+            current = WrapAngle(current);
+            target = WrapAngle(target);
+            var delta = 180 - Abs(Abs(current - target) - 180);
+            target = current + delta;
+            return SmoothDamp(current, target, ref velocity, smoothTime, deltaTime);
+        }
+
+        private static float WrapAngle(float angle) => ((angle % 360) + 360) % 360;
+        private static double WrapAngle(double angle) => ((angle % 360) + 360) % 360;
     }
 }
